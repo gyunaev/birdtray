b0091081f76a15cd28441c96c2a2ccc0802ee238
Thunder <thunder2@users.noreply.github.com>
2019-09-08 14:13:48 +0200

    Added portable save of settings in file for Windows, when birdtray.ini file exist

--- src/settings.cpp
+++ src/settings.cpp
@@ -1,6 +1,9 @@
 #include <QStandardPaths>
 #include <QBuffer>
 #include <QDir>
+#ifdef Q_OS_WIN
+#include <QCoreApplication>
+#endif
 
 #include "settings.h"
 #include "utils.h"
@@ -42,76 +45,95 @@ Settings::Settings(bool verboseOutput)
     mNewEmailMenuEnabled = false;
 }
 
+static QSettings *createSettings()
+{
+#ifdef Q_OS_WIN
+    QFileInfo applicationFilePath(qApp->applicationFilePath());
+    QFileInfo fileInfo( QDir( qApp->applicationDirPath() ), QString( "%1.ini" ).arg( applicationFilePath.baseName() ) );
+
+    if ( QFileInfo::exists(fileInfo.absoluteFilePath()) )
+        return new QSettings( fileInfo.absoluteFilePath(), QSettings::IniFormat );
+#endif
+
+    return new QSettings;
+}
+
 void Settings::save()
 {
-    QSettings settings;
+    QSettings *settings = createSettings();
+    if ( !settings )
+        return;
 
-    settings.setValue("common/notificationfont", mNotificationFont.toString() );
-    settings.setValue("common/defaultcolor", mNotificationDefaultColor.name() );
-    settings.setValue("common/profilepath", mThunderbirdFolderPath );
-    settings.setValue("common/blinkspeed", mBlinkSpeed );
-    settings.setValue("common/showhidethunderbird", mShowHideThunderbird );
-    settings.setValue("common/launchthunderbird", mLaunchThunderbird );
-    settings.setValue("common/hidewhenminimized", mHideWhenMinimized );
-    settings.setValue("common/exitthunderbirdonquit", mExitThunderbirdWhenQuit );
-    settings.setValue("common/restartthunderbird", mRestartThunderbird );
-    settings.setValue("common/notificationfontweight", mNotificationFontWeight );
-    settings.setValue("common/monitorthunderbirdwindow", mMonitorThunderbirdWindow );
-    settings.setValue("common/hidewhenstarted", mHideWhenStarted );
-    settings.setValue("common/hidewhenrestarted", mHideWhenRestarted );
-    settings.setValue("common/allowsuppressingunread", mAllowSuppressingUnreads );
-    settings.setValue("common/launchthunderbirddelay", mLaunchThunderbirdDelay );
-    settings.setValue("common/showunreademailcount", mShowUnreadEmailCount );
+    settings->setValue("common/notificationfont", mNotificationFont.toString() );
+    settings->setValue("common/defaultcolor", mNotificationDefaultColor.name() );
+    settings->setValue("common/profilepath", mThunderbirdFolderPath );
+    settings->setValue("common/blinkspeed", mBlinkSpeed );
+    settings->setValue("common/showhidethunderbird", mShowHideThunderbird );
+    settings->setValue("common/launchthunderbird", mLaunchThunderbird );
+    settings->setValue("common/hidewhenminimized", mHideWhenMinimized );
+    settings->setValue("common/exitthunderbirdonquit", mExitThunderbirdWhenQuit );
+    settings->setValue("common/restartthunderbird", mRestartThunderbird );
+    settings->setValue("common/notificationfontweight", mNotificationFontWeight );
+    settings->setValue("common/monitorthunderbirdwindow", mMonitorThunderbirdWindow );
+    settings->setValue("common/hidewhenstarted", mHideWhenStarted );
+    settings->setValue("common/hidewhenrestarted", mHideWhenRestarted );
+    settings->setValue("common/allowsuppressingunread", mAllowSuppressingUnreads );
+    settings->setValue("common/launchthunderbirddelay", mLaunchThunderbirdDelay );
+    settings->setValue("common/showunreademailcount", mShowUnreadEmailCount );
 
-    settings.setValue("advanced/tbcmdline", mThunderbirdCmdLine );
-    settings.setValue("advanced/tbwindowmatch", mThunderbirdWindowMatch );
-    settings.setValue("advanced/unreadmorkparser", mUseMorkParser );
-    settings.setValue("advanced/notificationfontminsize", mNotificationMinimumFontSize );
-    settings.setValue("advanced/notificationfontmaxsize", mNotificationMaximumFontSize );
-    settings.setValue("advanced/watchfiletimeout", mWatchFileTimeout );
-    settings.setValue("advanced/blinkingusealpha", mBlinkingUseAlphaTransition );
-    settings.setValue("advanced/unreadopacitylevel", mUnreadOpacityLevel );
+    settings->setValue("advanced/tbcmdline", mThunderbirdCmdLine );
+    settings->setValue("advanced/tbwindowmatch", mThunderbirdWindowMatch );
+    settings->setValue("advanced/unreadmorkparser", mUseMorkParser );
+    settings->setValue("advanced/notificationfontminsize", mNotificationMinimumFontSize );
+    settings->setValue("advanced/notificationfontmaxsize", mNotificationMaximumFontSize );
+    settings->setValue("advanced/watchfiletimeout", mWatchFileTimeout );
+    settings->setValue("advanced/blinkingusealpha", mBlinkingUseAlphaTransition );
+    settings->setValue("advanced/unreadopacitylevel", mUnreadOpacityLevel );
 
     // Convert the map into settings
-    settings.setValue("accounts/count", mFolderNotificationColors.size() );
+    settings->setValue("accounts/count", mFolderNotificationColors.size() );
     int index = 0;
 
     for ( QString uri : mFolderNotificationList )
     {
         QString entry = "accounts/account" + QString::number( index );
-        settings.setValue( entry + "Color", mFolderNotificationColors[uri].name() );
-        settings.setValue( entry + "URI", uri );
+        settings->setValue( entry + "Color", mFolderNotificationColors[uri].name() );
+        settings->setValue( entry + "URI", uri );
 
         index++;
     }
 
     // Convert new email data into settings
-    settings.setValue("newemail/enabled", mNewEmailMenuEnabled );
-    settings.setValue("newemail/count", mNewEmailData.size() );
+    settings->setValue("newemail/enabled", mNewEmailMenuEnabled );
+    settings->setValue("newemail/count", mNewEmailData.size() );
 
     for ( index = 0; index < mNewEmailData.size(); index++ )
     {
         QString entry = "newemail/id" + QString::number( index );
-        settings.setValue( entry, mNewEmailData[index].toByteArray() );
+        settings->setValue( entry, mNewEmailData[index].toByteArray() );
     }
 
     if ( !mNotificationIconUnread.isNull() )
-        savePixmap( settings, "common/notificationiconunread", mNotificationIconUnread );
+        savePixmap( *settings, "common/notificationiconunread", mNotificationIconUnread );
     else
-        settings.remove( "common/notificationiconunread" );
+        settings->remove( "common/notificationiconunread" );
 
-    savePixmap( settings, "common/notificationicon", mNotificationIcon );
+    savePixmap( *settings, "common/notificationicon", mNotificationIcon );
+
+    delete(settings);
 }
 
 void Settings::load()
 {
-    QSettings settings;
+    QSettings *settings = createSettings();
+    if ( !settings )
+        return;
 
-    if ( settings.contains( "common/notificationfont" ) )
-        mNotificationFont.fromString( settings.value( "common/notificationfont", "" ).toString() );
+    if ( settings->contains( "common/notificationfont" ) )
+        mNotificationFont.fromString( settings->value( "common/notificationfont", "" ).toString() );
 
-    mNotificationIcon = loadPixmap( settings, "common/notificationicon" );
-    mNotificationIconUnread = loadPixmap( settings, "common/notificationiconunread" );
+    mNotificationIcon = loadPixmap( *settings, "common/notificationicon" );
+    mNotificationIconUnread = loadPixmap( *settings, "common/notificationiconunread" );
 
     if ( mNotificationIcon.isNull() )
     {
@@ -119,86 +141,88 @@ void Settings::load()
             Utils::fatal("Cannot load default system tray icon");
     }
 
-    mNotificationDefaultColor = QColor( settings.value(
+    mNotificationDefaultColor = QColor( settings->value(
             "common/defaultcolor", mNotificationDefaultColor.name() ).toString() );
-    mThunderbirdFolderPath = settings.value(
+    mThunderbirdFolderPath = settings->value(
             "common/profilepath", mThunderbirdFolderPath ).toString();
-    mBlinkSpeed = settings.value("common/blinkspeed", mBlinkSpeed ).toInt();
-    mShowHideThunderbird = settings.value(
+    mBlinkSpeed = settings->value("common/blinkspeed", mBlinkSpeed ).toInt();
+    mShowHideThunderbird = settings->value(
             "common/showhidethunderbird", mShowHideThunderbird ).toBool();
-    mLaunchThunderbird = settings.value("common/launchthunderbird", mLaunchThunderbird ).toBool();
-    mHideWhenMinimized = settings.value("common/hidewhenminimized", mHideWhenMinimized ).toBool();
-    mExitThunderbirdWhenQuit = settings.value(
+    mLaunchThunderbird = settings->value("common/launchthunderbird", mLaunchThunderbird ).toBool();
+    mHideWhenMinimized = settings->value("common/hidewhenminimized", mHideWhenMinimized ).toBool();
+    mExitThunderbirdWhenQuit = settings->value(
             "common/exitthunderbirdonquit", mExitThunderbirdWhenQuit ).toBool();
-    mNotificationFontWeight = qMin(99, settings.value(
+    mNotificationFontWeight = qMin(99, settings->value(
             "common/notificationfontweight", mNotificationFontWeight ).toInt());
-    mMonitorThunderbirdWindow = settings.value(
+    mMonitorThunderbirdWindow = settings->value(
             "common/monitorthunderbirdwindow", mMonitorThunderbirdWindow ).toBool();
-    mRestartThunderbird = settings.value(
+    mRestartThunderbird = settings->value(
             "common/restartthunderbird", mRestartThunderbird ).toBool();
-    mHideWhenStarted = settings.value("common/hidewhenstarted", mHideWhenStarted ).toBool();
-    mHideWhenRestarted = settings.value("common/hidewhenrestarted", mHideWhenRestarted ).toBool();
-    mAllowSuppressingUnreads = settings.value(
+    mHideWhenStarted = settings->value("common/hidewhenstarted", mHideWhenStarted ).toBool();
+    mHideWhenRestarted = settings->value("common/hidewhenrestarted", mHideWhenRestarted ).toBool();
+    mAllowSuppressingUnreads = settings->value(
             "common/allowsuppressingunread", mAllowSuppressingUnreads ).toBool();
-    mLaunchThunderbirdDelay = settings.value(
+    mLaunchThunderbirdDelay = settings->value(
             "common/launchthunderbirddelay", mLaunchThunderbirdDelay ).toInt();
-    mShowUnreadEmailCount = settings.value(
+    mShowUnreadEmailCount = settings->value(
             "common/showunreademailcount", mShowUnreadEmailCount ).toBool();
 
-    mThunderbirdCmdLine = settings.value("advanced/tbcmdline", mThunderbirdCmdLine).toString();
-    mThunderbirdWindowMatch = settings.value(
+    mThunderbirdCmdLine = settings->value("advanced/tbcmdline", mThunderbirdCmdLine).toString();
+    mThunderbirdWindowMatch = settings->value(
             "advanced/tbwindowmatch", mThunderbirdWindowMatch ).toString();
-    mNotificationMinimumFontSize = settings.value(
+    mNotificationMinimumFontSize = settings->value(
             "advanced/notificationfontminsize", mNotificationMinimumFontSize ).toInt();
-    mNotificationMaximumFontSize = settings.value(
+    mNotificationMaximumFontSize = settings->value(
             "advanced/notificationfontmaxsize", mNotificationMaximumFontSize ).toInt();
-    mUseMorkParser = settings.value("advanced/unreadmorkparser", mUseMorkParser ).toBool();
-    mWatchFileTimeout = settings.value("advanced/watchfiletimeout", mWatchFileTimeout ).toUInt();
-    mBlinkingUseAlphaTransition = settings.value(
+    mUseMorkParser = settings->value("advanced/unreadmorkparser", mUseMorkParser ).toBool();
+    mWatchFileTimeout = settings->value("advanced/watchfiletimeout", mWatchFileTimeout ).toUInt();
+    mBlinkingUseAlphaTransition = settings->value(
             "advanced/blinkingusealpha", mBlinkingUseAlphaTransition ).toBool();
-    mUnreadOpacityLevel = settings.value(
+    mUnreadOpacityLevel = settings->value(
             "advanced/unreadopacitylevel", mUnreadOpacityLevel ).toDouble();
 
     mFolderNotificationColors.clear();
 
     // Convert the map into settings
-    int total = settings.value("accounts/count", 0 ).toInt();
+    int total = settings->value("accounts/count", 0 ).toInt();
 
     for ( int index = 0; index < total; index++ )
     {
         QString entry = "accounts/account" + QString::number( index );
-        QString key = settings.value( entry + "URI", "" ).toString();
+        QString key = settings->value( entry + "URI", "" ).toString();
         while (key.isEmpty() && index < total) {
             Utils::debug("Removing invalid account %d", index);
             QString lastEntry = "accounts/account" + QString::number( total - 1 );
             if (index != total - 1) {
-                key = settings.value(lastEntry + "URI", "").toString();
-                settings.setValue(entry + "URI", key);
-                settings.setValue(entry + "Color",
-                        settings.value(lastEntry + "Color", "").toString());
+                key = settings->value(lastEntry + "URI", "").toString();
+                settings->setValue(entry + "URI", key);
+                settings->setValue(entry + "Color",
+                        settings->value(lastEntry + "Color", "").toString());
             }
-            settings.remove(lastEntry + "URI");
-            settings.remove(lastEntry + "Color");
-            settings.setValue("accounts/count", --total);
+            settings->remove(lastEntry + "URI");
+            settings->remove(lastEntry + "Color");
+            settings->setValue("accounts/count", --total);
         }
         if (key.isEmpty()) {
             break;
         }
-        mFolderNotificationColors[ key ] = QColor( settings.value( entry + "Color", "" ).toString() );
+        mFolderNotificationColors[ key ] = QColor( settings->value( entry + "Color", "" ).toString() );
         mFolderNotificationList.push_back( key );
     }
 
     // Load new email data from settings
-    mNewEmailMenuEnabled = settings.value("newemail/enabled", mNewEmailMenuEnabled ).toBool();
+    mNewEmailMenuEnabled = settings->value("newemail/enabled", mNewEmailMenuEnabled ).toBool();
 
     mNewEmailData.clear();
-    total = settings.value("newemail/count", 0 ).toInt();
+    total = settings->value("newemail/count", 0 ).toInt();
 
     for ( int index = 0; index < total; index++ )
     {
         QString entry = "newemail/id" + QString::number( index );
-        mNewEmailData.push_back( Setting_NewEmail::fromByteArray( settings.value( entry, "" ).toByteArray() ) );
+        mNewEmailData.push_back( Setting_NewEmail::fromByteArray( settings->value( entry, "" ).toByteArray() ) );
     }
+
+    delete(settings);
 }
 
 QString Settings::getThunderbirdExecutablePath()
