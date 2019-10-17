32101c5f08aa37501522280295b0e9dce0fe77f1
Abestanis <abestanis.gc@gmail.com>
2019-10-17 16:37:12 +0200

    Handle the case when the total number of bytes to download is unknown

--- src/updatedownloaddialog.cpp
+++ src/updatedownloaddialog.cpp
@@ -39,6 +39,10 @@ void UpdateDownloadDialog::reset() {
 void UpdateDownloadDialog::onDownloadComplete() {
     downloadCompleted = true;
     ui->progressBar->hide();
+#ifdef Q_OS_WIN
+    taskBarButton.progress()->setRange(0, 100);
+    taskBarButton.progress()->setValue(100);
+#endif
     actionButton->setText(tr("Update and Restart"));
     ui->label->setText(tr("Download finished. Restart and update Birdtray?"));
     show();
@@ -47,13 +51,23 @@ void UpdateDownloadDialog::onDownloadComplete() {
 }
 
 void UpdateDownloadDialog::onDownloadProgress(qint64 bytesReceived, qint64 bytesTotal) {
+    if (bytesTotal <= 0) {
+        ui->label->setText(tr("Downloading Birdtray installer..."));
+        ui->progressBar->setRange(0, 0);
+#ifdef Q_OS_WIN
+        taskBarButton.progress()->setRange(0, 0);
+#endif
+        return;
+    }
     ui->label->setText(
             tr("Downloading Birdtray installer... (%1 Mb/ %2 Mb).")
                     .arg(qRound(bytesReceived / 1000000.0))
                     .arg(qRound(bytesTotal / 1000000.0)));
     int percent = qRound((bytesReceived / (double) bytesTotal) * 100.0);
+    ui->progressBar->setRange(0, 100);
     ui->progressBar->setValue(percent);
 #ifdef Q_OS_WIN
+    taskBarButton.progress()->setRange(0, 100);
     taskBarButton.progress()->setValue(percent);
 #endif
 }
