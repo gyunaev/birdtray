9e88b3cb7bc0f5c6cff0fb544b42139c1c2dcc0c
Abestanis <abestanis.gc@gmail.com>
2020-01-30 17:57:59 +0100

    Attach ot the parent console if the --debug parameter is present on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include <QApplication>
 #include "birdtrayapp.h"
-
+#ifdef Q_OS_WIN
+#  include <windows.h>
+#  include <cstdio>
+#  include <cstring>
+#endif /* Q_OS_WIN  */
 
 int main(int argc, char *argv[]) {
+#ifdef Q_OS_WIN
+    for (int i = 1; i < argc; i++) {
+        if (strcmp("--debug", argv[i]) == 0) {
+            AttachConsole(ATTACH_PARENT_PROCESS);
+            // reopen the std I/O streams to redirect I/O to the parents console.
+            freopen("CON", "w", stdout);
+            freopen("CON", "w", stderr);
+            freopen("CON", "r", stdin);
+            break;
+        }
+    }
+#endif /* Q_OS_WIN */
     BirdtrayApp app(argc, argv);
     return QApplication::exec();
 }
