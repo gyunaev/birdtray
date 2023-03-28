775aaa95436a045d05eef7ee36b7f5b7ae068dde
Amerey <info@amerey.eu>
2023-03-28 18:11:14 +0200

    czech translation - installer

--- src/translations/installer_cs.ts
+++ src/translations/installer_cs.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US">
+<TS version="2.1" language="cs">
 <context>
     <name>WindowsInstaller</name>
     <message>
@@ -10,157 +10,163 @@
     </message>
     <message>
         <source>A free system tray notification for new mail for Thunderbird.</source>
-        <translation>BirdtraySectionDescription</translation>
+        <translation>Upozornění na novou poštu v systémové liště pro Thunderbird.</translation>
     </message>
     <message>
         <source>Windows integration</source>
-        <translation>WinIntegrationSectionName</translation>
+        <translation>Integrace Windows</translation>
     </message>
     <message>
         <source>Select how to integrate the program in Windows.</source>
-        <translation>WinIntegrationGroupDescription</translation>
+        <translation>Vyberte způsob integrace programu do systému Windows.</translation>
     </message>
     <message>
         <source>AutoRun</source>
-        <translation>AutoRunSectionName</translation>
+        <translation>Automatické spouštění</translation>
     </message>
     <message>
         <source>Automatically start ${PRODUCT_NAME} after login.</source>
-        <translation>AutoRunDescription</translation>
+        <translation>Automaticky spustit ${PRODUCT_NAME} po přihlášení.</translation>
     </message>
     <message>
         <source>Auto Update-Check</source>
-        <translation>AutoCheckUpdateSectionName</translation>
+        <translation>Automatická aktualizace – kontrola</translation>
     </message>
     <message>
         <source>Automatically search for updates of ${PRODUCT_NAME} at startup.</source>
-        <translation>AutoCheckUpdateDescription</translation>
+        <translation>Automaticky vyhledat aktualizace ${PRODUCT_NAME} při spuštění.</translation>
     </message>
     <message>
         <source>Program Group Entry</source>
-        <translation>ProgramGroupSectionName</translation>
+        <translation>Vytvoření skupiny v programech</translation>
     </message>
     <message>
-        <source>Create a ${PRODUCT_NAME} program group under Start Menu > Programs.</source>
-        <translation>ProgramGroupDescription</translation>
+        <source>Create a ${PRODUCT_NAME} program group under Start Menu &gt; Programs.</source>
+        <translation>V nabídce Start &gt; Programy vytvořte skupinu programů ${PRODUCT_NAME}.</translation>
     </message>
     <message>
         <source>Desktop Entry</source>
-        <translation>DesktopEntrySectionName</translation>
+        <translation>Položka na ploše</translation>
     </message>
     <message>
         <source>Create a ${PRODUCT_NAME} icon on the Desktop.</source>
-        <translation>DesktopEntryDescription</translation>
+        <translation>Vytvořit ikonu ${PRODUCT_NAME} na ploše.</translation>
     </message>
     <message>
         <source>Start Menu Entry</source>
-        <translation>StartMenuSectionName</translation>
+        <translation>Nabídka Start</translation>
     </message>
     <message>
         <source>Create a ${PRODUCT_NAME} icon in the Start Menu.</source>
-        <translation>StartMenuDescription</translation>
+        <translation>Vytvořte ikonu ${PRODUCT_NAME} v nabídce Start.</translation>
     </message>
     <message>
         <source>Translations</source>
-        <translation>TranslationsSectionName</translation>
+        <translation>Překlady</translation>
     </message>
     <message>
         <source>Select translations for ${PRODUCT_NAME}. ${PRODUCT_NAME} will choose a translation based on the system language.</source>
-        <translation>TranslationsDescription</translation>
+        <translation>Vyberte překlady pro ${PRODUCT_NAME}. ${PRODUCT_NAME} vybere překlad podle jazyka systému.</translation>
     </message>
     <message>
         <source>${PRODUCT_NAME} User Settings</source>
-        <translation>UserSettingsSectionName</translation>
+        <translation>Uživatelská nastavení ${PRODUCT_NAME}</translation>
     </message>
     <message>
         <source>Your settings and configuration made within ${PRODUCT_NAME}.</source>
-        <translation>UserSettingsDescription</translation>
+        <translation>Vaše nastavení a konfigurace provedené v rámci ${PRODUCT_NAME}.</translation>
     </message>
     <message>
         <source>License Agreement</source>
-        <translation>LicenseStartMenuLinkName</translation>
+        <translation>Licenční smlouva</translation>
     </message>
     <message>
         <source>Start ${PRODUCT_NAME}</source>
-        <translation>FinishPageRunBirdtray</translation>
+        <translation>Spustit ${PRODUCT_NAME}</translation>
     </message>
     <message>
         <source>Click uninstall to begin the process.</source>
-        <translation>UninstallerConfirmation</translation>
+        <translation>Proces zahájíte kliknutím na tlačítko odinstalovat.</translation>
     </message>
     <message>
         <source>Uninstalling previous version of ${PRODUCT_NAME}...</source>
-        <translation>UninstallPreviousVersion</translation>
+        <translation>Probíhá odinstalace předchozí verze ${PRODUCT_NAME}...</translation>
     </message>
     <message>
         <source>Uninstalling the old ${PRODUCT_NAME} installation failed! Continuing will delete EVERYTHING in $3.</source>
-        <translation>UninstallPreviousVersionError</translation>
+        <translation>Odinstalace staré instalace ${PRODUCT_NAME} se nezdařila! Pokračováním smažete VŠECHNO v $3.</translation>
     </message>
     <message>
         <source>Error executing uninstaller.</source>
-        <translation>UninstallExecutionError</translation>
+        <translation>Chyba při odinstalaci.</translation>
     </message>
     <message>
         <source>${PRODUCT_NAME} is currently running. Press OK to stop ${PRODUCT_NAME} to continue with the installation.</source>
-        <translation>StopBirdtray</translation>
+        <translation>${PRODUCT_NAME} aktuálně běží. Stisknutím tlačítka OK zastavte ${PRODUCT_NAME} a pokračujte v instalaci.</translation>
     </message>
     <message>
         <source>Closing process ${EXE_NAME}...</source>
-        <translation>StoppingBirdtray</translation>
+        <translation>Ukončování procesu ${EXE_NAME}...</translation>
     </message>
     <message>
         <source>Failed to close ${PRODUCT_NAME}. Please retry or quit ${PRODUCT_NAME} manually to continue with the installation.</source>
-        <translation>StopBirdtrayError</translation>
+        <translation>${PRODUCT_NAME} se nepodařilo zavřít. Chcete-li pokračovat v instalaci, zkuste to znovu nebo ručně ukončete ${PRODUCT_NAME}.</translation>
     </message>
     <message>
         <source>${PRODUCT_NAME} is currently running. Press OK to stop ${PRODUCT_NAME} to continue with the uninstall.</source>
-        <translation>StopBirdtrayUninstall</translation>
+        <translation>${PRODUCT_NAME} je spuštěn. Chcete-li pokračovat v odinstalaci, stiskněte OK pro zastavení ${PRODUCT_NAME}.</translation>
     </message>
     <message>
         <source>Failed to close ${PRODUCT_NAME}. Please retry or quit ${PRODUCT_NAME} manually to continue with the uninstall.</source>
-        <translation>StopBirdtrayUninstallError</translation>
+        <translation>${PRODUCT_NAME} se nepodařilo zavřít. Chcete-li pokračovat v odinstalaci, zkuste to znovu nebo ručně ukončete ${PRODUCT_NAME}.</translation>
     </message>
     <message>
         <source>The setup of ${PRODUCT_NAME} is already running.
 Please, close all instances of it and click Retry to continue, or Cancel to exit.</source>
-        <translation>SetupAlreadyRunning</translation>
+        <translation>Instalace ${PRODUCT_NAME} již běží.
+Zavřete prosím všechny jeho výskyty a pokračujte kliknutím na Opakovat, pro ukončení klikněte na Zrušit.</translation>
     </message>
     <message>
         <source>The uninstall of ${PRODUCT_NAME} is already running.
 Please, close all instances of it and click Retry to continue, or Cancel to exit.</source>
-        <translation>UninstallAlreadyRunning</translation>
+        <translation>Odinstalace ${PRODUCT_NAME} již probíhá.
+Zavřete prosím všechny jeho výskyty a pokračujte kliknutím na Opakovat, pro ukončení klikněte na Zrušit.</translation>
     </message>
     <message>
         <source>The install path must not contain any of:
 ${BAD_PATH_CHARS}</source>
-        <translation>BadInstallPath</translation>
+        <translation>Instalační cesta nesmí obsahovat nic z:
+${BAD_PATH_CHARS}</translation>
     </message>
     <message>
         <source>The install path contains a previously installed ${PRODUCT_NAME} version, which will be replaced.</source>
-        <translation>InstallPathContainsPreviousVersion</translation>
+        <translation>Instalační cesta obsahuje dříve nainstalovanou verzi ${PRODUCT_NAME}, která bude nahrazena.</translation>
     </message>
     <message>
-        <source>The install path is not empty, but it doesn't look like it contains a previous ${PRODUCT_NAME} installation. The content will get overwritten.</source>
-        <translation>InstallPathNotEmpty</translation>
+        <source>The install path is not empty, but it doesn&apos;t look like it contains a previous ${PRODUCT_NAME} installation. The content will get overwritten.</source>
+        <translation>Instalační cesta není prázdná, ale nezdá se, že by obsahovala předchozí instalaci ${PRODUCT_NAME}. Obsah bude přepsán.</translation>
     </message>
     <message>
         <source>$0 (current user only)</source>
-        <translation>CurrentUserOnly</translation>
+        <translation>$0 (pouze aktuální uživatel)</translation>
     </message>
     <message>
         <source>${PRODUCT_NAME} ${VERSION} could not be fully installed.
 Please, restart Windows and run the setup program again.</source>
-        <translation>InstallFailed</translation>
+        <translation>${PRODUCT_NAME} ${VERSION} se nepodařilo plně nainstalovat.
+Restartujte Windows a znovu spusťte instalační program.</translation>
     </message>
     <message>
         <source>${PRODUCT_NAME} could not be fully installed.
 Please, restart Windows and run the setup program again.</source>
-        <translation>InstallFailedCauseUninstallerFailed</translation>
+        <translation>${PRODUCT_NAME} se nepodařilo plně nainstalovat.
+Restartujte Windows a znovu spusťte instalační program.</translation>
     </message>
     <message>
         <source>${PRODUCT_NAME} ${VERSION} could not be fully uninstalled.
 Please, restart Windows and run the uninstaller again.</source>
-        <translation>UninstallFailed</translation>
+        <translation>${PRODUCT_NAME} ${VERSION} se nepodařilo úplně odinstalovat.
+Restartujte prosím Windows a znovu spusťte odinstalační program.</translation>
     </message>
     <message>
         <source>Error deleting file:
@@ -169,125 +175,132 @@ $0
 
 Click Retry to try again, or
 Cancel to stop the uninstall.</source>
-        <translation>FileDeleteErrorDialog</translation>
+        <translation>Chyba při mazání souboru:
+
+0 $
+
+Klepněte na tlačítko Opakovat a zkuste to znovu, nebo
+Zrušením zastavíte odinstalaci.</translation>
     </message>
     <message>
         <source>Error deleting file $0</source>
-        <translation>FileDeleteError</translation>
+        <translation>Chyba při mazání souboru $0</translation>
     </message>
     <message>
         <source>You are about to install the 32-bit version of ${PRODUCT_NAME} on your 64-bit Windows. There is a 64-bit version of ${PRODUCT_NAME} available for download.</source>
-        <translation>Install64On32BitWarning</translation>
+        <translation>Chystáte se nainstalovat 32bitovou verzi produktu ${PRODUCT_NAME} do 64bitového systému Windows. Ke stažení je k dispozici 64bitová verze produktu ${PRODUCT_NAME}.</translation>
     </message>
     <message>
         <source>This installer contains the 64-bit version of ${PRODUCT_NAME}. Your computer is running a 32-bit version of Windows, which can not execute 64-bit programs. Please download the 32-bit installer of ${PRODUCT_NAME}.</source>
-        <translation>Install32On64BitError</translation>
+        <translation>Tento instalační program obsahuje 64bitovou verzi produktu ${PRODUCT_NAME}. Váš počítač používá 32bitovou verzi systému Windows, která nemůže spouštět 64bitové programy. Stáhněte si prosím 32bitový instalační program ${PRODUCT_NAME}.</translation>
     </message>
     <message>
         <source>This program requires at least Windows ${MIN_WINDOWS_VER}.</source>
-        <translation>UnsupportedWindowsVersionError</translation>
+        <translation>Tento program vyžaduje alespoň Windows ${MIN_WINDOWS_VER}.</translation>
     </message>
     <message>
         <source>Uninstalling ${PRODUCT_NAME} has been aborted. Please try again later.</source>
-        <translation>UninstallerAborted</translation>
+        <translation>Odinstalace ${PRODUCT_NAME} byla přerušena. Prosím zkuste to znovu později.</translation>
     </message>
     <message>
         <source>${PRODUCT_NAME} settings</source>
-        <translation>SettingsLink</translation>
+        <translation>Nastavení ${PRODUCT_NAME}</translation>
     </message>
     <message>
         <source>The uninstaller is started with administrator privileges but there seems to be no ${PRODUCT_NAME} installation for the administrator. Do you want to restart the uninstaller without admin privileges?</source>
-        <translation>UninstallRestartAsUserQuestion</translation>
+        <translation>Odinstalační program je spuštěn s právy správce, ale zdá se, že správce nemá žádnou instalaci produktu ${PRODUCT_NAME}. Chcete restartovat odinstalační program bez oprávnění správce?</translation>
     </message>
     <message>
         <source>Opening $0 in default browser...</source>
-        <translation>OpenUrl</translation>
+        <translation>Otevírání $0 ve výchozím prohlížeči...</translation>
     </message>
     <message>
         <source>Unable to find Visual C++ Runtime ${ARCH}.</source>
-        <translation>MissingVcRuntime</translation>
+        <translation>Nelze najít Visual C++ Runtime ${ARCH}.</translation>
     </message>
     <message>
         <source>The following required component was not found on this computer:
 Visual C++ Runtime ${ARCH}
 Do you want to download the installer for the component?</source>
-        <translation>MissingVcRuntime_Dialog</translation>
+        <translation>Následující požadovaná součást nebyla v tomto počítači nalezena:
+Visual C++ Runtime ${ARCH}
+Chcete si stáhnout instalační program součásti?</translation>
     </message>
     <message>
         <source>Once the download completes, please execute the downloaded installer. When the installer is finished, press OK to check the installation status again.</source>
-        <translation>MissingVcRuntime_Retry</translation>
+        <translation>Po dokončení stahování spusťte stažený instalační program. Po dokončení instalace stiskněte tlačítko OK a znovu zkontrolujte stav instalace.</translation>
     </message>
     <message>
         <source>The required component was successfully detected on your computer.</source>
-        <translation>MissingVcRuntime_Found</translation>
+        <translation>Požadovaná součást byla úspěšně detekována na vašem počítači.</translation>
     </message>
     <message>
         <source>The required component was not found on your computer. Please search online for Visual C++ ${ARCH} downloads for Visual Studio 2015 and higher.</source>
-        <translation>MissingVcRuntime_StillNotFound</translation>
+        <translation>Požadovaná součást nebyla ve vašem počítači nalezena. Stáhněte si z internetu Visual C++ ${ARCH} pro Visual Studio 2015 a vyšší.</translation>
     </message>
     <message>
         <source>${PRODUCT_NAME} will not be able to run.</source>
-        <translation>MissingVcRuntime_UnableToRun</translation>
+        <translation>${PRODUCT_NAME} nebude možné spustit.</translation>
     </message>
     <message>
         <source>${PRODUCT_NAME} will not be able to start because a required component is missing.</source>
-        <translation>MissingVcRuntime_UnableToRunDialog</translation>
+        <translation>${PRODUCT_NAME} nebude možné spustit, protože chybí požadovaná součást.</translation>
     </message>
     <message>
         <source>English</source>
-        <translation>Lang_en</translation>
+        <translation>Angličtina</translation>
     </message>
     <message>
         <source>French</source>
-        <translation>Lang_fr</translation>
+        <translation>Francouzština</translation>
     </message>
     <message>
         <source>German</source>
-        <translation>Lang_de</translation>
+        <translation>Němčina</translation>
     </message>
     <message>
         <source>Dutch</source>
-        <translation>Lang_nl</translation>
+        <translation>Nizozemština</translation>
     </message>
     <message>
         <source>Italian</source>
-        <translation>Lang_it</translation>
+        <translation>Italština</translation>
     </message>
     <message>
         <source>Russian</source>
-        <translation>Lang_ru</translation>
+        <translation>Ruština</translation>
     </message>
     <message>
         <source>Portugees</source>
-        <translation>Lang_pt</translation>
+        <translation>Portugalština</translation>
     </message>
     <message>
         <source>Spanish</source>
-        <translation>Lang_es</translation>
+        <translation>Španělština</translation>
     </message>
     <message>
         <source>Swedish</source>
-        <translation>Lang_sv</translation>
+        <translation>Švédština</translation>
     </message>
     <message>
         <source>Turkish</source>
-        <translation>Lang_tr</translation>
+        <translation>Turečtina</translation>
     </message>
     <message>
         <source>Polish</source>
-        <translation>Lang_pl</translation>
+        <translation>Polština</translation>
     </message>
     <message>
         <source>Chinese (Simplified)</source>
-        <translation>Lang_zh_cn</translation>
+        <translation>Zjednodušená čínština</translation>
     </message>
     <message>
         <source>Greek</source>
-        <translation>Lang_el</translation>
+        <translation>Řečtina</translation>
     </message>
     <message>
         <source>Czech</source>
-        <translation>Lang_cs</translation>
+        <translation>Čeština</translation>
     </message>
 </context>
 </TS>
